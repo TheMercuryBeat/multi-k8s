8c1240b9df560dfb1105cf119d9be0a7705ba9c8
Diego Fernandez <diego.fernandez@2getherbank.com>
2019-03-27 17:46:59 +0100

    New title

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calulator Version Placa Placa</h1>
+            <h1 className="App-title">Fib Calulator Version Mazapan pan</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
